c12e6fdd27af778ef1be46220c5f432d5126c128
Jeffrey Salmond <js947@cam.ac.uk>
2020-04-29 10:26:43 +0100

    clean up dolfinx package

--- packages/dolfinx/package.py
+++ packages/dolfinx/package.py
@@ -13,10 +13,7 @@ class Dolfinx(CMakePackage):
     url = "https://github.com/FEniCS/dolfinx"
     git = "https://github.com/FEniCS/dolfinx.git"
 
-    submodules = True
-
     version("master", branch="master")
-    #extends("python")
 
     variant("doc", default=False, description="Builds the documentation")
 
@@ -28,18 +25,17 @@ class Dolfinx(CMakePackage):
     depends_on("scotch+mpi~metis")
     depends_on("py-ufl")
     depends_on("py-ffcx")
+    depends_on("py-fiat")
     depends_on("py-sphinx@1.0.1:", when="+doc", type="build")
+    depends_on("pkgconfig")
 
-    #depends_on("python@3.5:")
+    depends_on("python@3.5:")
 
     depends_on("cmake@3.9:", type="build")
 
     root_cmakelists_dir = "cpp"
 
     def cmake_args(self):
-        mpi = self.spec["mpi"]
         return [
             "-DDOLFIN_SKIP_BUILD_TESTS=True",
-            "-DMPI_C_COMPILER=%s" % mpi.mpicc,
-            "-DMPI_CXX_COMPILER=%s" % mpi.mpicxx,
         ]
